62fa40732ab9102837f53279019d523dda8d0f0f
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2026-03-09 02:18:54 +0200

    asdaaaaasa

--- app/main.py
+++ app/main.py
@@ -10,6 +10,9 @@ def copy_file(command: str) -> None:
     if source_file_name == destination_file_name:
         return
 
-    with open(source_file_name, "r") as source_file, \
-            open(destination_file_name, "w") as destination_file:
-        destination_file.write(source_file.read())
+    try:
+        with open(source_file_name, "r") as source_file, \
+                open(destination_file_name, "w") as destination_file:
+            destination_file.write(source_file.read())
+    except FileNotFoundError:
+        return
