e871416b4c8b08b28dc93eb64ff23bd64e3ffcf5
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2026-03-09 02:17:27 +0200

    asdaaaaas

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file_name = parts[1]
+    destination_file_name = parts[2]
+
+    if source_file_name == destination_file_name:
+        return
+
+    with open(source_file_name, "r") as source_file, \
+            open(destination_file_name, "w") as destination_file:
+        destination_file.write(source_file.read())
